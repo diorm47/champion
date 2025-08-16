927886effb208c5ca4d7f110ead0143c2d8bd9f0
diorm47 <dior47m@gmail.com>
2025-08-16 16:06:11 +0500

    active btn

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,17 @@ const questions = document.querySelectorAll(".quiz_question");
 const loadingFinal = document.querySelector(".quiz_result_loading");
 let currentStep = 0;
 
+document.querySelectorAll(".quiz_answers").forEach((block) => {
+  const buttons = block.querySelectorAll(".quiz_btn");
+
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      buttons.forEach((b) => b.classList.remove("active_quiz_btn"));
+      btn.classList.add("active_quiz_btn");
+    });
+  });
+});
+
 document.querySelectorAll(".quiz_btn").forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const currentQuestion = questions[currentStep];
