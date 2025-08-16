90cfddc543d057ed1ff914113b1e8309d1392b59
diorm47 <dior47m@gmail.com>
2025-08-16 15:43:06 +0500

    animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,29 @@
 const questions = document.querySelectorAll(".quiz_question");
-const loading = document.querySelector(".quiz_result_loading");
-
+const loadingFinal = document.querySelector(".quiz_result_loading");
 let currentStep = 0;
 
 document.querySelectorAll(".quiz_btn").forEach((btn) => {
-  btn.addEventListener("click", () => {
-    questions[currentStep].classList.remove("active");
+  btn.addEventListener("click", (e) => {
+    const currentQuestion = questions[currentStep];
+    const loadingBlock = currentQuestion.querySelector(".loading_block");
 
-    currentStep++;
-    if (currentStep < questions.length) {
-      questions[currentStep].classList.add("active");
-    } else {
-      loading.classList.add("active");
+    loadingBlock.style.display = "flex";
 
-      setTimeout(() => {
-        window.location.href = "https://championautoinsurance.com/#";
-      }, 1000);
-    }
+    setTimeout(() => {
+      loadingBlock.style.display = "none";
+
+      currentQuestion.classList.remove("active");
+      currentStep++;
+
+      if (currentStep < questions.length) {
+        questions[currentStep].classList.add("active");
+      } else {
+        loadingFinal.classList.add("active");
+
+        setTimeout(() => {
+          window.location.href = "https://championautoinsurance.com/#";
+        }, 1000);
+      }
+    }, 400);
   });
 });
